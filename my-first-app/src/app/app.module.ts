35da9d426a3e3a761a4fa2905db7b30b2d0b3c7e
Nan Saung Chan Aye <nansaungchanaye1991@gmail.com>
2022-07-15 12:45:52 +0630

    First Initial Commit

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
